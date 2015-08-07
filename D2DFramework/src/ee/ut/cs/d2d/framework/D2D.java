cf79ffcbbcf005e8eb64f8df80a92693af5c5e94
Huber Flores <kyoraul@gmail.com>
2015-08-07 14:25:18 +0300

    bluetooth state machine as a service

--- D2DFramework/src/ee/ut/cs/d2d/framework/D2D.java
+++ D2DFramework/src/ee/ut/cs/d2d/framework/D2D.java
@@ -11,20 +11,23 @@
 
 package ee.ut.cs.d2d.framework;
 
-
-import ee.ut.cs.d2d.network.D2DBluetooth;
 import ee.ut.cs.d2d.network.D2DBluetoothActions;
+import ee.ut.cs.d2d.services.D2DMeshService;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -48,16 +51,20 @@ public class D2D extends Activity{
 	
 	D2DBluetoothActions btReceiver = new D2DBluetoothActions(this);
 	private BluetoothAdapter btAdapter;
-	D2DBluetooth btDevice;
+	//D2DBluetooth btDevice;
+	
+	private D2DMeshService meshService; 
+	Context context;
 	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 	    setContentView(R.layout.d2d_activity_main);
-	
+	    context = this;
+	    
 	    btAdapter = BluetoothAdapter.getDefaultAdapter();
-	    btDevice = new D2DBluetooth(this);
+	    //btDevice = new D2DBluetooth(this);
 	     
 	    outputScreen = (TextView)findViewById(R.id.outputTextViewer);
 	     
@@ -67,10 +74,20 @@ public class D2D extends Activity{
 			public void onClick(View v) {
 				try {
 					if (btAdapter.isEnabled()) {
-						btDevice.D2DOff();
+						if (meshService!=null){
+							meshService.btOff();
+						}else{
+							Log.d(TAG, "service is not connected");
+						}
+						//btDevice.D2DOff();
 					} else {
 						if (!btAdapter.isEnabled()){
-							btDevice.D2DOn();
+							if (meshService!=null){
+								meshService.btOn();
+							}else{
+								Log.d(TAG, "service is not connected");
+							}
+							//btDevice.D2DOn();
 						}
 					}
 				} catch (Exception e) {
@@ -86,6 +103,7 @@ public class D2D extends Activity{
 				try {
 					Toast.makeText(getBaseContext(), "Cleaning screen..", Toast.LENGTH_SHORT).show();
 					clearOutputScreen();
+					meshService.print();
 			
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -99,7 +117,12 @@ public class D2D extends Activity{
 				public void onClick(View v) {
 					try {
 						Toast.makeText(getBaseContext(), "Showing logs..", Toast.LENGTH_SHORT).show();
-						btDevice.D2DDiscovery();
+						if (meshService!=null){
+							meshService.btDiscovery();
+						}else{
+							Log.d(TAG, "service is not connected");
+						}
+						//btDevice.D2DDiscovery();
 				
 					} catch (Exception e) {
 						e.printStackTrace();
@@ -155,6 +178,9 @@ public class D2D extends Activity{
 	     filter = new IntentFilter(D2D.OUTPUT_SCREEN_EVENT);
 	     screenReceiver = new MessageReceiver();
 	     registerReceiver(screenReceiver, filter);
+	     
+	     Intent intent= new Intent(this, D2DMeshService.class);
+	     bindService(intent, meshConnection, Context.BIND_AUTO_CREATE);
 
 	 }
 	 
@@ -163,6 +189,12 @@ public class D2D extends Activity{
 		super.onDestroy();
 		unregisterReceiver(btReceiver);
 		unregisterReceiver(screenReceiver);
+	 }
+	 
+	 @Override
+	 public void onPause(){
+		 super.onPause();
+		 unbindService(meshConnection);
 	 }
 	 
 	 public class MessageReceiver extends BroadcastReceiver {
@@ -198,6 +230,27 @@ public class D2D extends Activity{
 	 public void showLogFiles(){
 	    	
 	 }
+	 
+	 
+	 private ServiceConnection meshConnection = new ServiceConnection() {
+
+			@Override
+			public void onServiceConnected(ComponentName className, IBinder binder) {
+				D2DMeshService.MyBinder connector = (D2DMeshService.MyBinder) binder;
+
+				meshService = connector.getMeshServiceInstance();
+				meshService.setServiceContext(context);
+				
+				
+				Toast.makeText(D2D.this, "Connected", Toast.LENGTH_SHORT)
+						.show();
+			}
+
+			@Override
+			public void onServiceDisconnected(ComponentName className) {
+				meshService = null;
+			}
+	 };
 	 
 	 
 	 
--- D2DFramework/src/ee/ut/cs/d2d/services/D2DMeshService.java
+++ D2DFramework/src/ee/ut/cs/d2d/services/D2DMeshService.java
@@ -1,6 +1,8 @@
 package ee.ut.cs.d2d.services;
 
+import ee.ut.cs.d2d.network.D2DBluetooth;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
@@ -12,9 +14,12 @@ public class D2DMeshService extends Service {
 	
 	private final IBinder mBinder = new MyBinder();
 	
+	D2DBluetooth btDevice = null;
+	
 	@Override  
 	public int onStartCommand(Intent intent, int flags, int startId) {
-		
+		Log.d(TAG, "onStartCommand");
+			
 		return Service.START_NOT_STICKY;
 	}
 	
@@ -33,8 +38,28 @@ public class D2DMeshService extends Service {
 	}
 	
 	
+	public void btOn(){
+		btDevice.D2DOn();
+	}
+	
+	public void btOff(){
+		btDevice.D2DOff();
+	}
+	
+	public void btDiscovery(){
+		btDevice.D2DDiscovery();
+	}
+	
 	public void print(){
 		Log.d(TAG, "Service method called");
+	}
+	
+	public void setServiceContext(Context context){
+		if (btDevice==null){
+			Log.d(TAG, "device instance is null");
+			btDevice = new D2DBluetooth(context);
+		}
+		
 	}
 
 }
